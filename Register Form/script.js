a2eddc3bc44926d43dfbeacc74b7374f69dd386d
NAVEEN KUMAR <nk946432@gmail.com>
2024-09-01 16:31:32 +0530

    but it not works

--- Register Form/script.js
+++ Register Form/script.js
@@ -12,8 +12,58 @@ form.addEventListener('submit',(event)=>{
 });
 
 function validateInput(){
-  const usernameValue =username.ariaValueMax.trim();
-  const emailValue =email.ariaValueMax.trim();
-  const passwordValue =password.ariaValueMax.trim();
-  const cpasswordValue =cpassword.ariaValueMax.trim();
+  const usernameValue =username.value.trim();
+  const emailValue =email.value.trim();
+  const passwordValue =password.value.trim();
+  const cpasswordValue =cpassword.value.trim();
+
+  if(usernameValue==''){
+    setError(username,'Username is required')
+  }
+  else{
+    setSuccess(username);
+  }
+  if(emailValue==''){
+    setError(email,'username is required')
+  }
+  else{
+    setSuccess(email);
+  }
+  if(password==''){
+    setError(password,"password required")
+  }
+  else if(password.length<8){
+    setError(password,'password is not in the order')
+  }
+  else{
+    setSuccess(password)
+  }
+  if(cpassword==''){
+    setError(cpassword,'conform password is required')
+  }
+  else if(cpassword!==password){
+    setError(cpassword, 'conform passowrd does not match')
+  }
+  else{
+    setSuccess(cpassword)
+  }
+
 }     
+
+
+function setError(element,message){
+  const inputGroup= element.parentElement;
+  const errorElement=inputGroup.querySelector('.error');
+  message=errorElement.innerText;
+  inputGroup.classList.add('error');
+  inputGroup.classList.remove('success')
+}
+
+function setSuccess(element){
+  const inputGroup= element.parentElement;
+  const errorElement=inputGroup.querySelector('.error');
+  errorElement.innerText='';
+  
+  inputGroup.classList.add('success');
+  inputGroup.classList.remove('error')
+}
