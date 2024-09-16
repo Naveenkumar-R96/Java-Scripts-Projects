0f3ddda71fabc8599d867cc322668ea044fca42c
NAVEEN KUMAR <nk946432@gmail.com>
2024-09-16 11:03:36 +0530

    created a reviews

--- 03-reviews/setup/app.js
+++ 03-reviews/setup/app.js
@@ -30,55 +30,53 @@ const reviews = [
   },
 ];
 // select items
-const img = document.getElementById('person-img');
-const author = document.getElementById('author');
-const job = document.getElementById('job');
-const info = document.getElementById('info');
+const img=document.querySelector("#person-img");
+const author=document.querySelector("#author");
+const job=document.querySelector("#job");
+const information=document.querySelector("#info")
 
-const prevBtn = document.querySelector('.prev-btn');
-const nextBtn = document.querySelector('.next-btn');
-const randomBtn = document.querySelector('.random-btn');
+const nxtButton=document.querySelector(".next-btn");
+const prevButton=document.querySelector(".prev-btn");
+const randomButton = document.querySelector('.random-btn');
 
 // set starting item
-let currentItem = 0;
 
+ let currentItem=0;
 // load initial item
-window.addEventListener('DOMContentLoaded', function () {
-  const item = reviews[currentItem];
-  img.src = item.img;
-  author.textContent = item.name;
-  job.textContent = item.job;
-  info.textContent = item.text;
+window.addEventListener("DOMContentLoaded",function(){
+  showPerson();
 });
 
 // show person based on item
-function showPerson(person) {
-  const item = reviews[person];
-  img.src = item.img;
-  author.textContent = item.name;
-  job.textContent = item.job;
-  info.textContent = item.text;
-}
-// show next person
-nextBtn.addEventListener('click', function () {
-  currentItem++;
-  if (currentItem > reviews.length - 1) {
-    currentItem = 0;
-  }
-  showPerson(currentItem);
-});
-// show prev person
-prevBtn.addEventListener('click', function () {
-  currentItem--;
-  if (currentItem < 0) {
-    currentItem = reviews.length - 1;
-  }
-  showPerson(currentItem);
-});
-// show random person
-randomBtn.addEventListener('click', function () {
-  console.log('hello');
+function showPerson(){
+  const item=reviews[currentItem];
+  img.src  =item.img;
+  author.textContent=item.name;
+  job.textContent=item.job;
+  information.textContent=item.text;
 
-  currentItem = Math.floor(Math.random() * reviews.length);
-  showPerson(currentItem);
-});
+}
+
+// show next person
+nxtButton.addEventListener('click',function(){
+  currentItem++;
+  if(currentItem> reviews.length-1){
+    currentItem=0;
+  }
+  showPerson();
+})
+
+// show prev person
+prevButton.addEventListener('click',function(){
+  currentItem--;
+  if(currentItem<0){
+    currentItem=reviews.length-1;
+  }
+  showPerson();
+})
+
+// show random person
+randomButton.addEventListener('click',function(){
+  currentItem=Math.floor(Math.random() *reviews.length)
+  showPerson();
+})
